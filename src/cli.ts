886ba200315b96996ce30eb97e929e8fdbf15a0f
Гера Шмидт <MarmeladOFForiginal@mail.ru>
2022-10-02 19:53:11 +0500

    homework

--- src/cli.ts
+++ src/cli.ts
@@ -4,9 +4,11 @@ import HelpCommand from "./cli-commands/help-command.js";
 import VersionCommand from "./cli-commands/version-command.js";
 import CLIApplication from "./cli-application/cli-application.js";
 import ImportCommand from "./cli-commands/import-command.js";
+import GenerateCommand from "./cli-commands/generate-command.js";
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
-    new HelpCommand,new VersionCommand, new ImportCommand
+    new HelpCommand,new VersionCommand,
+    new ImportCommand, new GenerateCommand
 ])
 myManager.processCommand(process.argv)
